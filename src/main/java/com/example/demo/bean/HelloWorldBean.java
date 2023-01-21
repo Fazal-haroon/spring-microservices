646e85f8e523da158e2c9157fd57db0458876f0c
Fazal Haroon <kharoon187@gmail.com>
2023-01-21 19:12:38 +0400

    Hello World Rest API with Spring Boot to return a Bean

---
+++ src/main/java/com/example/demo/bean/HelloWorldBean.java
@@ -0,0 +1,27 @@
+package com.example.demo.bean;
+
+public class HelloWorldBean {
+    private String message;
+
+    public HelloWorldBean() {
+    }
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,10 @@ public class HelloWorldController {
     @GetMapping("/hello-world")
     public String helloWorld(){
         return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World Bean");
     }
 }
