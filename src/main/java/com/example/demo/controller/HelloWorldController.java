4398c074a3b4b62b7bbfa40dcf53abd3306e7588
Fazal Haroon <kharoon187@gmail.com>
2023-01-21 23:14:04 +0400

    Hello World Rest API with Spring Boot @PathVariable

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Rest API
@@ -9,12 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     // /hello-world
     @GetMapping("/hello-world")
-    public String helloWorld(){
+    public String helloWorld() {
         return "Hello World";
     }
 
     @GetMapping(path = "/hello-world-bean")
-    public HelloWorldBean helloWorldBean(){
+    public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World Bean");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean("Hello World Path1, "+ name);
+    }
+
+    @GetMapping(path = "/hello-world/path-variable2/{name}")
+    public HelloWorldBean helloWorldPathVariable2(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World Path2, %s", name));
     }
 }
