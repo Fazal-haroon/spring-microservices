6f2c2645644443ffc3a72deb6fbcbb71c3c4164b
Fazal Haroon <kharoon187@gmail.com>
2023-01-22 00:15:39 +0400

    Get Retrieve Method for specific User Resource

--- src/main/java/com/example/demo/user/UserResource.java
+++ src/main/java/com/example/demo/user/UserResource.java
@@ -1,6 +1,7 @@
 package com.example.demo.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,5 +19,10 @@ public class UserResource {
     @GetMapping("/users")
     public List<User> retrieveAllUsers(){
         return userDaoService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User findUser(@PathVariable int id) {
+        return userDaoService.findOne(id);
     }
 }
