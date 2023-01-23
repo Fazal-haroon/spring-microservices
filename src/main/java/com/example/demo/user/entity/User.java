c804d269d41cc8a14a15efa0e40fdfd7bec2df4c
Fazal Haroon <kharoon187@gmail.com>
2023-01-24 01:10:54 +0400

    Customize field names in response using @JsonProperty

--- src/main/java/com/example/demo/user/entity/User.java
+++ src/main/java/com/example/demo/user/entity/User.java
@@ -1,5 +1,6 @@
 package com.example.demo.user.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -9,9 +10,11 @@ public class User {
     private Integer id;
 
     @Size(min = 2, message = "Name should hava at least 2 characters")
+    @JsonProperty("user_name")
     private String name;
 
     @Past(message = "Birth Date should be in the past")
+    @JsonProperty("birth_date")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
