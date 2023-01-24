b9493f188d97ab0dcd8f9e4b84fa83f1189f1b38
Fazal Haroon <kharoon187@gmail.com>
2023-01-24 23:08:50 +0400

    Connecting H2 using jpa and hibernate, creating user entity and some test data in schema.sql

--- src/main/java/com/example/demo/user/entity/User.java
+++ src/main/java/com/example/demo/user/entity/User.java
@@ -1,12 +1,18 @@
 package com.example.demo.user.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min = 2, message = "Name should hava at least 2 characters")
@@ -16,6 +22,9 @@ public class User {
     @Past(message = "Birth Date should be in the past")
     @JsonProperty("birth_date")
     private LocalDate birthDate;
+
+    public User() {
+    }
 
     public User(Integer id, String name, LocalDate birthDate) {
         this.id = id;
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,2 @@
+insert into USER_DETAILS(id, birth_date, name) values(1, current_date(), 'fazal' );
+insert into USER_DETAILS(id, birth_date, name) values(2, '1998-01-12', 'haroon' );
