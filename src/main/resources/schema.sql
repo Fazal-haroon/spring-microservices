cc2cf6d538d951668857db9ada8a8dbae4c7874f
Fazal Haroon <kharoon187@gmail.com>
2023-01-24 23:54:51 +0400

    Minor changes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
 insert into USER_DETAILS(id, birth_date, name)
-values (1, current_date(), 'fazal');
+values (1, current_date(), 'Fazal');
 
 insert into USER_DETAILS(id, birth_date, name)
-values (2, '1998-01-12', 'haroon');
+values (2, '1998-01-12', 'Haroon');
 
 insert into USER_DETAILS(id, birth_date, name)
-values (3, '1975-03-14', 'fazal alim');
+values (3, '1975-03-14', 'Fazal Alim');
 
 insert into post(id, description, user_id)
 values (1, 'I want to learn AWS', 1);
