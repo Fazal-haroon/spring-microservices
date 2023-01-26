e9d886ca5305f343cd2d1f4b9c486e92e2ae1aac
Fazal Haroon <kharoon187@gmail.com>
2023-01-26 16:42:35 +0400

    Creating a hard coded limits service

---
+++ 02.Microservice-2x-Spring-boot/limits-service/src/main/java/com/example/limitsservice/LimitConfiguration.java
@@ -0,0 +1,30 @@
+package com.example.limitsservice;
+
+public class LimitConfiguration {
+    private Integer maximum;
+    private Integer minimum;
+
+    public LimitConfiguration() {
+    }
+
+    public LimitConfiguration(Integer maximum, Integer minimum) {
+        this.maximum = maximum;
+        this.minimum = minimum;
+    }
+
+    public Integer getMaximum() {
+        return maximum;
+    }
+
+    public Integer getMinimum() {
+        return minimum;
+    }
+
+    @Override
+    public String toString() {
+        return "LimitConfiguration{" +
+                "maximum=" + maximum +
+                ", minimum=" + minimum +
+                '}';
+    }
+}
---
+++ 02.Microservice-2x-Spring-boot/limits-service/src/main/java/com/example/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,14 @@
+package com.example.limitsservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsConfigurationController {
+
+    @GetMapping("/limits")
+    public LimitConfiguration retrieveLimitsFromConfiguration(){
+        return new LimitConfiguration(1000, 1);
+    }
+
+}
