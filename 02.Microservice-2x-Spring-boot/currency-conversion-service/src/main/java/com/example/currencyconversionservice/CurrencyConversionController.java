163408bfa82c39186875ff919ec8efff71ebd403
Fazal Haroon <kharoon187@gmail.com>
2023-01-28 16:09:18 +0400

    Creating a service for currency conversion

---
+++ 02.Microservice-2x-Spring-boot/currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,16 @@
+package com.example.currencyconversionservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyConversionController {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+        return new CurrencyConversionBean(1L, from, to, BigDecimal.ONE, quantity, quantity, 0);
+    }
+}
