3ee664129a09b6cb2c7665ce028412f3eebc94dc
Fazal Haroon <kharoon187@gmail.com>
2023-01-28 15:03:21 +0400

    configure JPA and initialized data

--- 02.Microservice-2x-Spring-boot/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/ExchangeValue.java
+++ 02.Microservice-2x-Spring-boot/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/ExchangeValue.java
@@ -1,10 +1,19 @@
 package com.example.currencyexchangeservice;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "Exchange_Value")
 public class ExchangeValue {
+    @Id
     private Long id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private int port;
---
+++ 02.Microservice-2x-Spring-boot/currency-exchange-service/src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+insert into Exchange_Value(id, currency_from, currency_to, conversion_multiple, port)
+values (1001L, 'USD', 'PKR', 250, 0);
+
+insert into Exchange_Value(id, currency_from, currency_to, conversion_multiple, port)
+values (1002L, 'AED', 'PKR', 62, 0);
+
+insert into Exchange_Value(id, currency_from, currency_to, conversion_multiple, port)
+values (1003L, 'SAR', 'PKR', 61, 0);
