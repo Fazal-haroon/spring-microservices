41794ad179c0c9cc7e000e42bc9432822f2cfd06
Fazal Haroon <kharoon187@gmail.com>
2023-01-28 12:42:06 +0400

    Create a simple hard coded currency exchange service usd to pkr

---
+++ 02.Microservice-2x-Spring-boot/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,16 @@
+package com.example.currencyexchangeservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(250));
+    }
+}
