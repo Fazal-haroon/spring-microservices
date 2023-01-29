84706a566dca2cfa47ec5b05162c90f35ccb43e5
Fazal Haroon <kharoon187@gmail.com>
2023-01-29 23:30:18 +0400

    connecting Limits-Service, Currency-Conversion-Service, and Currency-Exchange-Service microservices to Eureka

--- 02.Microservice-2x-Spring-boot/currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ 02.Microservice-2x-Spring-boot/currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -4,6 +4,7 @@ import com.example.currencyconversionservice.config.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableFeignClients("com.example.currencyconversionservice")
 //@RibbonClient(name = "currency-exchange-service", configuration = RibbonConfiguration.class)
+@EnableEurekaClient
 public class CurrencyConversionServiceApplication {
 
 	@Bean
