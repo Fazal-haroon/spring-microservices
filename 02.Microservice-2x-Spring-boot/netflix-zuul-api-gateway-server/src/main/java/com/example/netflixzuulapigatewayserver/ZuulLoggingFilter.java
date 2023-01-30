556eb9d6f75db45743badc7c1a8a4894aae1ee71
Fazal Haroon <kharoon187@gmail.com>
2023-01-30 23:11:57 +0400

    Executing a request through Zuul API Gateway

---
+++ 02.Microservice-2x-Spring-boot/netflix-zuul-api-gateway-server/src/main/java/com/example/netflixzuulapigatewayserver/ZuulLoggingFilter.java
@@ -0,0 +1,38 @@
+package com.example.netflixzuulapigatewayserver;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+
+    private static Logger logger = LoggerFactory.getLogger(ZuulLoggingFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        logger.info("Request -> {} request uri -> {}", request, request.getRequestURI());
+        return null;
+    }
+}
