a5c0ea2fb1a9443dd69481f90da6debd7558819b
Fazal Haroon <kharoon187@gmail.com>
2023-02-02 01:22:17 +0400

    creating a hard coded limits service - v2

---
+++ 03.Microservice-v2-spring-boot/limits-service/src/main/java/com/example/limitsservice/controller/LimitsController.java
@@ -0,0 +1,21 @@
+package com.example.limitsservice.controller;
+
+import com.example.limitsservice.bean.Limits;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.limitsservice.configuration.Configuration;
+
+@RestController
+public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(configuration.getMinimum(),
+                configuration.getMaximum());
+//		return new Limits(1,1000);
+    }
+}
