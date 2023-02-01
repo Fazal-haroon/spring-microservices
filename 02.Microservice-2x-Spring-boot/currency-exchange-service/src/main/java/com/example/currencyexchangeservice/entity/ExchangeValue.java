44df9f2921a43abb99642da793c1202204b6d35c
Fazal Haroon <kharoon187@gmail.com>
2023-02-01 22:28:03 +0400

    Connecting microservices to Zipkin

--- 02.Microservice-2x-Spring-boot/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/entity/ExchangeValue.java
+++ 02.Microservice-2x-Spring-boot/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/entity/ExchangeValue.java
@@ -1,15 +1,14 @@
 package com.example.currencyexchangeservice.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "Exchange_Value")
 public class ExchangeValue {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
     @Column(name = "currency_from")
     private String from;
