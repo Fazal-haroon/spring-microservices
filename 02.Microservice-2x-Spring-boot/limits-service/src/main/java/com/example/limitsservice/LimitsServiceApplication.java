928f490f1485645eb0f19f836508768be27176b7
Fazal Haroon <kharoon187@gmail.com>
2023-02-01 23:21:12 +0400

    Fault Tolerance with Hystrix

--- 02.Microservice-2x-Spring-boot/limits-service/src/main/java/com/example/limitsservice/LimitsServiceApplication.java
+++ 02.Microservice-2x-Spring-boot/limits-service/src/main/java/com/example/limitsservice/LimitsServiceApplication.java
@@ -3,9 +3,11 @@ package com.example.limitsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
--- 02.Microservice-2x-Spring-boot/limits-service/src/main/java/com/example/limitsservice/controller/LimitsConfigurationController.java
+++ 02.Microservice-2x-Spring-boot/limits-service/src/main/java/com/example/limitsservice/controller/LimitsConfigurationController.java
@@ -2,6 +2,7 @@ package com.example.limitsservice.controller;
 
 import com.example.limitsservice.bean.LimitConfiguration;
 import com.example.limitsservice.config.Configuration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,16 @@ public class LimitsConfigurationController {
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfiguration(){
         return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+    }
+
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+    public LimitConfiguration retrieveConfiguration(){
+        throw new RuntimeException("Not Available");
+    }
+
+    public LimitConfiguration fallbackRetrieveConfiguration() {
+        return new LimitConfiguration(404, 404404);
     }
 
 }
