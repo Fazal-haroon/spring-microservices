982be9ee8ea952ed77f2c5dd5d237a49629bf7fc
Fazal Haroon <kharoon187@gmail.com>
2023-02-03 19:31:21 +0400

    Setting up Dynamic Port in the Response - V2

--- 03.Microservice-v2-spring-boot/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ 03.Microservice-v2-spring-boot/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.example.currencyexchangeservice.controller;
 
 import com.example.currencyexchangeservice.bean.CurrencyExchange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,15 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to){
-        return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
     }
 
 }
