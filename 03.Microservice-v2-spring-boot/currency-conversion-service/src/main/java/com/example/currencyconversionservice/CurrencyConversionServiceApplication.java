4bb98f20d45331b8b7740e5280f4da3139fbdc3a
Fazal Haroon <kharoon187@gmail.com>
2023-02-06 23:26:19 +0400

    connect currency conversion & currency exchange microservices

--- 03.Microservice-v2-spring-boot/currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ 03.Microservice-v2-spring-boot/currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class CurrencyConversionServiceApplication {
 
 	@Bean
--- 03.Microservice-v2-spring-boot/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ 03.Microservice-v2-spring-boot/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
