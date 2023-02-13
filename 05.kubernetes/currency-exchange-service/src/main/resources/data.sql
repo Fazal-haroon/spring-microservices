5321768121ed766dc42b8afd5208934747d83eba
Fazal Haroon <kharoon187@gmail.com>
2023-02-13 11:40:29 +0400

    Minor changes in kubernetes

--- 05.kubernetes/currency-exchange-service/src/main/resources/data.sql
+++ 05.kubernetes/currency-exchange-service/src/main/resources/data.sql
@@ -1,9 +1,9 @@
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(10001,'USD','INR',65,'');
+values(10001,'USD','PKR',65,'');
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(10002,'EUR','INR',75,'');
+values(10002,'AED','PKR',75,'');
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(10003,'AUD','INR',25,'');
+values(10003,'KSA','PKR',25,'');
