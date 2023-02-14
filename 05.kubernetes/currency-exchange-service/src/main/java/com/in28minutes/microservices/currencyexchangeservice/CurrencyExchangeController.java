dba5e52eab1f6140d5245328709c3e0cf5b0f4dd
Fazal Haroon <kharoon187@gmail.com>
2023-02-14 19:24:19 +0400

    creating environment variable to enable Microservices Communication

--- 05.kubernetes/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ 05.kubernetes/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -38,7 +38,7 @@ public class CurrencyExchangeController {
 		
 		//CHANGE-KUBERNETES
 		String host = environment.getProperty("HOSTNAME");
-		String version = "v11";
+		String version = "v12";
 		
 		currencyExchange.setEnvironment(port + " " + version + " " + host);
 		
